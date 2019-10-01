447d7912019be27c0d0cad61014d6925aebb6370
Liozart <bobponay@gmail.com>
2019-10-01 13:02:14 +0200

    first commit

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    bool plus = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            gameObject.transform.Rotate(0, 10 * Time.deltaTime, 0);
+    }
+}
